7b21b9034a9aa5280598c2f34ad7190fc9985f32
Andilemkhala <andilemkhala25@gmail.com>
2024-03-08 22:24:01 +0000

    0x02-ES6_classes

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,8 +1,6 @@
-//Prototype: export default class ClassRoom
-//It should accept one attribute named maxStudentsSize (Number) and 
-//assigned to _maxStudentsSize
+// 0-classroom.js
 export default class ClassRoom {
-  constructor(maxStudentsSize) {
-    this._maxStudentsSize = maxStudentsSize;
-  }
+    constructor(maxStudentsSize) {
+        this._maxStudentsSize = maxStudentsSize;
+    }
 }
